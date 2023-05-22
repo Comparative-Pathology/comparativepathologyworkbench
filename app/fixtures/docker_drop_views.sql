e8838ca57cfdf76a1ea884bfcdbb01dd5c1c10bf
Mike Wicks <Michael.Wicks@ed.ac.uk>
2023-05-22 12:28:40 +0100

    2023-05-22
    Ensure SQL files are not ignored by Git

---
+++ app/fixtures/docker_drop_views.sql
@@ -0,0 +1,4 @@
+DROP VIEW matrices_bench_summary;
+DROP VIEW matrices_collection_summary;
+DROP VIEW matrices_environment_summary;
+DROP VIEW matrices_image_summary;
---
+++ fixtures/docker_authorise_views.sql
@@ -0,0 +1,4 @@
+GRANT ALL ON ALL TABLES IN SCHEMA public TO workbench_czi;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO workbench_czi;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO workbench_czi;
+GRANT ALL PRIVILEGES ON DATABASE workbench_czi_prod to workbench_czi;
